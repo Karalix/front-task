09abd6b5beca351cfa2df854b3dae9479982c2a3
Alix Ducros <alix@krlx.fr>
2024-02-29 15:19:47 +0100

    correction bg-color image

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
   <div class="max-w-lg mx-auto py-20">
     <div class="flex items-center gap-3">
       <div class="bg-white rounded-full w-[88px] h-[88px] p-1 shrink-0" :class="{'inner-border': student.age != null}">
-        <img :src="student.image || ''" class="bg-white w-full h-full object-cover rounded-full" />
+        <img :src="student.image || ''" class="bg-gray-300 w-full h-full object-cover rounded-full" />
       </div>
       <div>
         <label for="hours-old" class="koulen-regular" :class="{'text-[#3D06D7]': student.age != null}">{{ student.name }} is</label>
